3f74b87e08280102ed80c195934307c94eb488b8
Piotr Gumkowski <efscar123@gmail.com>
2024-04-07 18:31:36 +0200

    h

---
+++ employees/Manager.java
@@ -0,0 +1,19 @@
+package employees;
+
+public class Manager extends Employee {
+    public int numberOfSubordinates;
+
+    public Manager(String fullName, double salary, int numberOfSubordinates) {
+        super(fullName, salary);
+        this.numberOfSubordinates = numberOfSubordinates;
+    }
+
+    public void setNumberOfSubordinates(int newNumberOfSubordinates) {
+        this.numberOfSubordinates = newNumberOfSubordinates;
+    }
+
+    @Override
+    public String toString() {
+        return "Manager: " + getName() + ", Salary: " + getSalary() + ", Subordinates: " + numberOfSubordinates;
+    }
+}
